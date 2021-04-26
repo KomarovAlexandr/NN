0f63e543ea7f6a77feccc791e5dda9bd6769ca8a
Александр Комаров <raptor.al.com@yandex.ru>
2021-04-26 23:01:35 +0300

    допилено задание на зачет

--- CNN/additional_func.py
+++ CNN/additional_func.py
@@ -1,30 +1,77 @@
-import PIL.ImageOps
 import cv2
-from matplotlib import image
 from os import listdir
-from PIL import Image
 import numpy as np
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from matplotlib import pyplot as plt
 
 
-def load_data():
+def load_data(shape, oneImage = 1, showFlag = 0):
     data = []
-    for filename in listdir("data"):
-        img = cv2.imread("data/" + filename, cv2.IMREAD_GRAYSCALE)
-        img = cv2.resize(img, (112, 112))
-        cv2.imshow("Image_1", img)
-        cv2.waitKey(1)
+    if oneImage:
+        for filename in listdir("data"):
+            img = cv2.imread("data/" + filename, cv2.IMREAD_GRAYSCALE)
+            img = cv2.resize(img, (112, 112))
+            if showFlag:
+                cv2.imshow("Image_1", img)
+                cv2.waitKey(1)
 
-        img = cv2.threshold(img, 110, 255, cv2.THRESH_BINARY_INV)[1]
-        kernel = np.ones((5, 5), 'uint8')
-        img = cv2.dilate(img, kernel)
-        img = cv2.blur(img, ksize=(3, 3))
+            img = cv2.threshold(img, 110, 255, cv2.THRESH_BINARY_INV)[1]
+            kernel = np.ones((5, 5), 'uint8')
+            img = cv2.dilate(img, kernel)
+            img = cv2.blur(img, ksize=(3, 3))
 
-        # scaleKoef = 1.0 / np.max(img)
-        # img = img * (1 + scaleKoef)
+            if showFlag:
+                cv2.imshow("Image_2", img)
+                cv2.waitKey(50)
+            img = cv2.resize(img, (shape[0], shape[1]))
+            data.append(img)
+    else:
+        for filename in listdir():
+            if filename.endswith("jpg"):
+                img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+                img = cv2.resize(img, (112, 112))
+                if showFlag:
+                    cv2.imshow("Image_1", img)
+                    cv2.waitKey(1)
 
-        cv2.imshow("Image_2", img)
-        cv2.waitKey(100)
-        img = cv2.resize(img, (28, 28))
-        data.append(img)
+                img = cv2.threshold(img, 110, 255, cv2.THRESH_BINARY_INV)[1]
+                kernel = np.ones((5, 5), 'uint8')
+                img = cv2.dilate(img, kernel)
+                img = cv2.blur(img, ksize=(3, 3))
+
+                if showFlag:
+                    cv2.imshow("Image_2", img)
+                    cv2.waitKey(50)
+                img = cv2.resize(img, (shape[0], shape[1]))
+                data.append(img)
+
+    data = np.array(data)
+    if showFlag and oneImage:
+        for i in range(0, 9):
+            plt.subplot(330 + 1 + i)
+            plt.imshow(data[i+1], cmap=plt.get_cmap('gray'))
+        plt.show()
+
+    data = data.reshape(data.shape[0], shape[0], shape[1], shape[2]).astype(float) / 255.0
+
+    for i in range(0, len(data)):
+        scaleKoef = 1.0 / np.max(data[i])
+        data[i] = data[i] * scaleKoef
     return data
+
+
+def getModel(shape):
+    model = Sequential()
+    model.add(Conv2D(30, (5, 5), activation='relu', input_shape=shape))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(15, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(10, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
 
--- CNN/main.py
+++ CNN/main.py
@@ -3,105 +3,82 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 from prettytable import PrettyTable
 from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Activation, BatchNormalization, AveragePooling2D
-from tensorflow.keras.optimizers import SGD, RMSprop, Adam
-import tensorflow_datasets as tfds
-import tensorflow as tf
-import logging
 import numpy as np
-import time
 from tensorflow.keras.datasets import mnist
 from matplotlib import pyplot as plt
 import additional_func as af
+import tensorflow as tf
 
+"""
+Флаги
+"""
+# Загрузить модели
+loadModelFlag = 1
+# Сохранение модели
+saveModelFlag = 0
+# Тест на произвольном изображении - 0, Тест на базе из 10 изображений от 0 до 9
+freeImageTestFlag = 0
+# Флаг отображения загруженных данных и данных для теста
+showFlag = 0
 
-data = np.array(af.load_data())
-for i in range(0, 9):
-    plt.subplot(330 + 1 + i)
-    plt.imshow(data[i+1], cmap=plt.get_cmap('gray'))
-plt.figure()
-data = data.reshape(data.shape[0], 28, 28, 1).astype(float) / 255.0
-
-for i in range(0, 10):
-    scaleKoef = 1.0 / np.max(data[i])
-    data[i] = data[i] * scaleKoef
-
-Nine_index = [4, 22, 33, 43, 45, 48, 54, 57, 87, 110]
-data_1 = []
-
-# Загрузка база
+# Загрузка
 (trainX, trainy), (testX, testy) = mnist.load_data()
-# 4 22 33 43 45 48 54 57 87 110
-for i in range(0, 9):
-    plt.subplot(330 + 1 + i)
-    #plt.imshow(trainX[i], cmap=plt.get_cmap('gray'))
-    data_1.append(trainX[Nine_index[i]])
-    plt.imshow(data_1[i], cmap=plt.get_cmap('gray'))
-data_1.append(trainX[Nine_index[9]])
-# plt.subplot(330 + 1 + 0)
-# plt.imshow(data[9], cmap=plt.get_cmap('gray'))
-# plt.subplot(330 + 1 + 1)
-# plt.imshow(data_1[9], cmap=plt.get_cmap('gray'))
-plt.show()
-
 # Вывод размерности данных
 print("Train: X=%s, y=%s" % (trainX.shape, trainy.shape))
 print("Test: X=%s, y=%s" % (testX.shape, testy.shape))
+if showFlag:
+	for i in range(0, 9):
+		plt.subplot(330 + 1 + i)
+		plt.imshow(trainX[i], cmap=plt.get_cmap('gray'))
+	plt.show()
+
 # Размер картинок
 imageSize_X = trainX.shape[1]
 imageSize_Y = trainX.shape[2]
+shape = (imageSize_X, imageSize_Y, 1)
 # Размерность выходного слоя
 classSize = 10
+
+testData = af.load_data(shape, oneImage=freeImageTestFlag, showFlag=showFlag)
+
 # Подготовка данных
-trainX = trainX.reshape(trainX.shape[0], imageSize_X, imageSize_Y, 1)
-trainX.astype('float32')
-trainX = trainX / 255.0
-testX = testX.reshape(testX.shape[0], imageSize_X, imageSize_Y, 1)
-testX.astype('float32')
-testX = testX / 255.0
+trainX = trainX.reshape(trainX.shape[0], shape[0], shape[1], shape[2]).astype(float) / 255.0
+testX = testX.reshape(testX.shape[0], shape[0], shape[1], shape[2]).astype(float) / 255.0
+
 trainy = keras.utils.to_categorical(trainy, classSize)
 testy = keras.utils.to_categorical(testy, classSize)
 
-data_1 = np.array(data_1)
-data_1 = data_1.reshape(data_1.shape[0], imageSize_X, imageSize_Y, 1)
-data_1.astype('float32')
-data_1 = data_1 / 255.0
+if loadModelFlag:
+	print("Load model ...")
+	model = tf.keras.models.load_model('model')
+	print(model.summary())
+else:
+	print("Create model ...")
+	model = af.getModel(shape)
+	print(model.summary())
+	print("Model Fit ...")
+	model.fit(trainX, trainy, epochs=10, batch_size=100, verbose=1, validation_data=(testX, testy))
 
-
-model = Sequential()
-model.add(Conv2D(30, (5, 5), activation='relu', input_shape=(imageSize_X, imageSize_Y, 1)))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(15, (3, 3), activation='relu', input_shape=(imageSize_X, imageSize_Y, 1)))
-model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.2))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dense(10, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-print(model.summary())
-print("Model Fit ...")
-model.fit(trainX, trainy, epochs=10, batch_size=100,
-					verbose=1, validation_data=(testX, testy))
 print("Model Predict ...")
-predict = model.predict(data)
+predict = model.predict(testData)
 
-t = PrettyTable(['Numb', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+if freeImageTestFlag:
+	table = PrettyTable(['Numb', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+	for i in range(0, 10):
+		pred = list()
+		for j in range(0, 10):
+			pred.append("%.2f%%" % (predict[i, j] * 100))
+		table.add_row([i, pred[0], pred[1], pred[2], pred[3],
+					   pred[4], pred[5], pred[6], pred[7], pred[8], pred[9]])
+	print(table)
+else:
+	percent = 0.0
+	predictNumber = 0
+	for i in range(0, 10):
+		if predict[0, i] > percent:
+			percent = predict[0, i]
+			predictNumber = i
+	print("This is", predictNumber, "  ", percent * 100, "%")
 
-for i in range(0, 10):
-	pred = list()
-	for j in range(0, 10):
-		pred.append("%.2f%%" % (predict[i,j] * 100))
-	t.add_row([i, pred[0],
-					pred[1],
-					pred[2],
-					pred[3],
-					pred[4],
-					pred[5],
-					pred[6],
-					pred[7],
-					pred[8],
-					pred[9]])
-print(t)
+if saveModelFlag:
+	model.save('model')
